ac48bae3fd0ca1acd442baea4a319e0cc170ebdc
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-17 19:24:06 +0530

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -1,44 +1,46 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
 
-int isArmstrong(int num) {
-  int originalNum, remainder, result = 0, n = 0;
-  originalNum = num;
-  
-  // count number of digits
-  while (originalNum != 0) {
-    originalNum /= 10;
-    ++n;
+int isArmstrong(int number) 
+{
+
+  // declare variables
+  int lastDigit = 0;
+  int power = 0;
+  int sum = 0;
+
+  // temporary variable to store number
+  int n = number;
+
+  while(n!=0) {
+
+     // find last digit
+     lastDigit = n % 10;
+
+     // find power of digit (order = 3)
+     power = lastDigit*lastDigit*lastDigit;
+
+     // add power value into sum
+     sum += power;
+
+     // remove last digit
+     n /= 10;
   }
-  
-  originalNum = num;
-  
-  // calculate result
-  while (originalNum != 0) {
-    remainder = originalNum % 10;
-    result += pow(remainder, n);
-    originalNum /= 10;
-  }
-  
-  // check if result is equal to the original number
-  if (result == num) {
-    return 1;
-  } else {
-    return 0;
-  }
+
+  if(sum == number) return 0;
+  else return 1;
 }
 
-int main() {
-  int num;
-  
-  printf("Enter a number: ");
-  scanf("%d", &num);
-  
-  if (isArmstrong(num)) {
-    printf("%d is an Armstrong number.", num);
-  } else {
-    printf("%d is not an Armstrong number.", num);
-  }
-  
+int main()
+{
+  int number;
+
+  printf("Enter number: ");
+  scanf("%d",&number);
+
+  if(isArmstrong(number) == 0)
+  printf("%d is an Armstrong number.\n", number);
+  else
+  printf("%d is not an Armstrong number.", number);
+
   return 0;
 }
