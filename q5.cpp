4863f86b71be736e78587d2d3f3763c8ea96195c
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-03-17 19:13:31 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int isArmstrong(int num) {
+  int originalNum, remainder, result = 0, n = 0;
+  originalNum = num;
+  
+  // count number of digits
+  while (originalNum != 0) {
+    originalNum /= 10;
+    ++n;
+  }
+  
+  originalNum = num;
+  
+  // calculate result
+  while (originalNum != 0) {
+    remainder = originalNum % 10;
+    result += pow(remainder, n);
+    originalNum /= 10;
+  }
+  
+  // check if result is equal to the original number
+  if (result == num) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int num;
+  
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  
+  if (isArmstrong(num)) {
+    printf("%d is an Armstrong number.", num);
+  } else {
+    printf("%d is not an Armstrong number.", num);
+  }
+  
+  return 0;
+}
